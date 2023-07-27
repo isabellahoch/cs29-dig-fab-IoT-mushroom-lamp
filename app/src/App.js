9685663ffbcd93c13111257416f7d41745c5d5ab
isabellahoch <40366749+isabellahoch@users.noreply.github.com>
2023-07-27 11:07:41 -0400

    create frontend

---
+++ app/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './App.css';
+import WebSocketClient from './WebSocketClient';
+
+function App() {
+  const [isLightOn, setLightOn] = useState(false);
+
+  // Function to handle the button click and toggle the light status
+  const handleToggleLight = () => {
+    setLightOn((prevIsLightOn) => !prevIsLightOn);
+  };
+
+  return (
+    <div className="App">
+      <button onClick={handleToggleLight}>
+        <div style={{alignItems: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column', height:'100vh'}}>
+          {isLightOn ? (
+            <img src={require('./assets/mushroom_ON.svg').default} alt="Mushroom ON" />
+          ) : (
+            <img src={require('./assets/mushroom_OFF.svg').default} alt="Mushroom OFF" />
+          )}
+        </div>
+      </button>
+      <WebSocketClient isLightOn={isLightOn} />
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/WebSocketClient.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useRef } from 'react';
+
+const WebSocketClient = ({ isLightOn }) => {
+  // Create a ref to hold the WebSocket instance
+  const socketRef = useRef(null);
+
+  useEffect(() => {
+    // Create a new WebSocket connection
+    const socket = new WebSocket('ws://localhost:8080'); // Change the URL if needed
+    socketRef.current = socket;
+
+    // Event: when the connection is established
+    socket.onopen = () => {
+      console.log('Connected to WebSocket server (React client)');
+    };
+
+    // Event: when an error occurs
+    socket.onerror = (error) => {
+      console.error('WebSocket error:', error);
+    };
+
+    // Cleanup function to close the WebSocket connection when the component unmounts
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  useEffect(() => {
+    // Function to send ON/OFF messages to the WebSocket server
+    const sendLightStatus = () => {
+      // Check if the WebSocket is open before sending the message
+      if (socketRef.current && socketRef.current.readyState === WebSocket.OPEN) {
+        const message = isLightOn ? 'ON' : 'OFF';
+        console.log('Sending message:', message);
+        socketRef.current.send(message);
+      }
+    };
+
+    // Send ON/OFF messages whenever the button state changes
+    sendLightStatus();
+  }, [isLightOn]);
+
+  return null; // This component doesn't render anything
+};
+
+export default WebSocketClient;
